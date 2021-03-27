ee66622e2d5dd024fe7d8fe45d67d4c5ab03fce7
choosk <77675898+ladjeek-actions@users.noreply.github.com>
2021-03-27 11:21:46 +0800

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+echo "\n";
+echo "html>\n";
+echo "head>Live2d api已经成功运行</head>\n";
+echo "body>\n";
+echo "center><h1>此页面仅为了让Leancloud运行</h1></center>\n";
+echo "/body>\n";
+echo "/html>\n";
+?>
